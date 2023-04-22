69acdaf5a4f74ce5124838cdbe601ddf5878ac30
patrickfosso49 <patrickfosso49@gmail.com>
2023-04-22 11:26:17 +0100

    generalised User and CustomUserDetails class added

---
+++ src/main/java/com/projet/classwork/model/User.java
@@ -0,0 +1,41 @@
+package com.projet.classwork.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class User {
+   
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String registrationNumber;
+
+    @Column(nullable = false, length = 80)
+    private String firstName;
+
+    @Column(nullable = false , length = 80)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public String getRole() {
+        return this.getClass().getSimpleName();
+    }
+}
