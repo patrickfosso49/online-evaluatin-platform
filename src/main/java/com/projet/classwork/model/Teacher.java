760c5896e9487364c0e2bd04a59db93fb7b22ac0
patrickfosso49 <patrickfosso49@gmail.com>
2023-04-28 12:04:53 +0100

    Teacher model fully updated

--- src/main/java/com/projet/classwork/model/Teacher.java
+++ src/main/java/com/projet/classwork/model/Teacher.java
@@ -1,13 +1,27 @@
 package com.projet.classwork.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Entity
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class Teacher extends User {
    
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "teacher", cascade = CascadeType.MERGE)
+    private Collection<Class> classes = new ArrayList<>();
 
 }
