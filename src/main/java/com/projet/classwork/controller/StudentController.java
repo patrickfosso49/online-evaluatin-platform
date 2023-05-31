7a7bcdab8930ffd3a469ac9275594af1da6f6c2f
patrickfosso49 <patrickfosso49@gmail.com>
2023-05-31 18:03:49 +0100

    minor bug fixed on StudentController

--- src/main/java/com/projet/classwork/controller/StudentController.java
+++ src/main/java/com/projet/classwork/controller/StudentController.java
@@ -1,15 +1,12 @@
 package com.projet.classwork.controller;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projet.classwork.service.ClasseService;
-import com.projet.classwork.service.EvaluationService;
-import com.projet.classwork.service.PropositionService;
+import com.projet.classwork.service.MailService;
 import com.projet.classwork.service.StudentService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,40 +15,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.projet.classwork.dto.SubmissionDto;
 import com.projet.classwork.model.Classe;
 import com.projet.classwork.model.Evaluation;
-import com.projet.classwork.model.Proposition;
-import com.projet.classwork.model.Questionnaire;
 import com.projet.classwork.model.Student;
 import com.projet.classwork.model.Submission;
 import com.projet.classwork.service.SubmissionService;
-import com.projet.classwork.service.QuestionnaireService;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
     private final StudentService studentService;
-    private final QuestionnaireService questionnaireService;
     private final SubmissionService submissionService;
-    private final PropositionService propositionService;
-    private final EvaluationService evaluationService;
     private final ClasseService classeService;
-
-
-    
-    public StudentController(StudentService studentService,QuestionnaireService questionnaireService, PropositionService propositionService,
-        SubmissionService submissionService, EvaluationService evaluationService, ClasseService classeService) {
-                
-           this.studentService = studentService;
-           this.questionnaireService = questionnaireService;
-           this.propositionService = propositionService;
-           this.submissionService = submissionService;
-           this.evaluationService = evaluationService;
-           this.classeService = classeService;
-    }
-
+    private final MailService mailService; 
 
     @PostMapping("")
     public ResponseEntity<?> create (@RequestBody Student student) {
@@ -74,7 +54,10 @@ public class StudentController {
                 if (evaluation.getQuestionnaire().getId() == questionnaireId) {
                     Submission body = submissionService.create(submission, id, questionnaireId);
 
-                    return (body == null) ? ResponseEntity.internalServerError().build() : ResponseEntity.created(null).body(body);
+                    if (body == null) {
+                        return ResponseEntity.badRequest().build();
+                    }
+                    mailService.sendEmail(student.getEmail(), evaluation.getTitle(), "Hi, your mark is available for this evaluation on classwork"); 
                 }
             }
         } 
@@ -101,10 +84,17 @@ public class StudentController {
     @PutMapping("/{id}/classes/{classeId}")
     public ResponseEntity<?> joinClasse(@PathVariable("id") Long id, @PathVariable("classeId") Long classeId) {
 
-        Student body = studentService.findById(id);
-        Classe classe = classeService.findById(classeId);
-         
-        body = studentService.save(body);
+        Student student = studentService.findById(id);
+        
+        if(student == null) return ResponseEntity.badRequest().build();
+        Student body = null;
+        for (Classe classe: student.getClasses()) {
+            if (classe.getId() == classeId) {
+                classe.addStudent(student);
+                body = studentService.save(student);
+                
+            }
+        }
 
         if (body == null) return ResponseEntity.badRequest().build();
 
@@ -112,6 +102,14 @@ public class StudentController {
 
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable("id") Long id, @RequestBody Student student) {
+        Student body = studentService.findById(id);
+        body.setEmail(student.getEmail());
+        body = studentService.save(body);
+        if(body == null){ return ResponseEntity.badRequest().build();}
 
+        return ResponseEntity.ok(body);
+    }
 
 }
